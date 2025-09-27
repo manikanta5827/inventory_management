49e4df8b1763e50ff1b2aa5b8be273c205b0fda3
Manikanta Thummuri <postbox5827@gmail.com>
2025-09-27 19:43:29 +0530

    feat: implement error handler

---
+++ src/middlewares/error.middleware.js
@@ -0,0 +1,29 @@
+import logger from "../utils/logger.utils.js";
+import { parse } from 'stack-trace';
+import path from "path";
+
+const errorHandler = (err, req, res) => {
+    let errorMessage = err.message;
+    const stackFrames = parse(err);
+
+    if (stackFrames[0]) {
+        let fileName = path.basename(stackFrames[0].getFileName() ?? 'fileNotfound');
+        let lineNumber = stackFrames[0].getLineNumber();
+        let functionName = stackFrames[0].getFunctionName();
+
+        let logMessage = `[CRITICAL]: Error happened in file::${fileName} Line::${lineNumber} FunctionName::${functionName} Message::${errorMessage}`;
+
+        logger.warn(logMessage);
+    }
+
+    if (errorMessage.includes("Can't reach database server")) {
+        errorMessage = "Database connection failed. Please verify that your database server is running and accessible."
+    }
+    res.status(500).send({
+        status: "error",
+        message: 'Something went wrong!',
+        error: errorMessage
+    });
+}
+
+export default errorHandler;
