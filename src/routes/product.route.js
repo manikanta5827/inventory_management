0cb8692336f45ac3e0289fe78bd8fec0bff9a2cc
Manikanta Thummuri <postbox5827@gmail.com>
2025-09-27 21:26:55 +0530

    update: implement api's on product crud

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,7 +1,20 @@
 import { Router } from "express";
-import { createProduct } from "../controllers/product.controller.js";
+import { createProduct, deleteProduct, getAllProducts, getProduct, updateProduct } from "../controllers/product.controller.js";
 const router = Router();
 
+// create product
 router.post('/', createProduct);
+
+// get all products
+router.get('/', getAllProducts);
+
+// get single product
+router.get('/:id', getProduct);
+
+// update product
+router.put('/:id', updateProduct);
+
+// delete product
+router.delete('/:id', deleteProduct);
 
 export default router;
